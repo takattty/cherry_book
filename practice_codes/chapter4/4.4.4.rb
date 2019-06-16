58fe03c5522ce2c0a261154065835217e8d2af44
takattty <r9r_celtics_t9t@icloud.com>
2019-06-16 20:05:19 +0900

    inject/reduceメソッド

--- practice_codes/chapter4/4.4.4.rb
+++ practice_codes/chapter4/4.4.4.rb
@@ -13,3 +13,5 @@ puts sums
 
 ok = ['ta', 'ka', 'ty'].inject('Mynaneis') { |result, n| result + n}
 puts ok
+#まずはinjectの引数が初回のみの値。次からは、nに各要素が入っていく。それがresultの演算を行なって結果は戻り値としてresultに入る。
+#これを繰り返して最後まで計算。最後はrejectメソッドの返り血となる。
