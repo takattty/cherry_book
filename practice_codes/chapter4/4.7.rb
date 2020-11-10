ee72b3bc45b6521f6403150a05d20819a9492df2
takattty <r9r_celtics_t9t@icloud.com>
2020-11-10 10:35:14 +0900

    practice about array

---
+++ practice_codes/chapter4/4.7.rb
@@ -0,0 +1,28 @@
+# About Array
+a = [1, 2, 3, 4, 5]
+p a[1, 3] # index, length
+p a.values_at(0, 2, 4) # index
+p a.first
+p a.last
+
+# Change Value
+i = [1, 2, 3]
+p i[0] = 4
+p i << 5
+p i.push(6)
+p i.delete(5)
+p i
+
+u = [1, 2]
+e = [1, 3, 4, 6, 8]
+p u.concat(e)
+p u, e
+p u + e
+p u, e
+
+o, *k = 100, 200, 300
+p o, k # K=Array
+
+p %w(apple melon orange) # => ["apple", "melon", "orange"] クォーテーションやカンマを入れないで済むので短縮。
+
+p 'ruby'.chars
