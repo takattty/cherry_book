be0374dbd46c46a4471463b4634640c6535aff10
takattty <r9r_celtics_t9t@icloud.com>
2020-12-24 23:10:22 +0900

    learn symbol

--- practice_codes/chapter5/5.rb
+++ practice_codes/chapter5/5.rb
@@ -37,3 +37,31 @@ p array1.to_s
 array2 = {:ja=>"yen", :us=>"dollar", :india=>"rupee"}
 p array2.to_a
 p array2.to_s
+
+
+# イミュータブルなオブジェクトなので初期化でブロックを使い、破壊的な変更の意図しないバグを防ぐ
+hash = Hash.new { 'hey' }
+p hash["a"] # => "hey"
+p hash # => {}
+
+# hashには何も入ってないんだ？？？あ、空のままって書かれてるわ。
+
+p %s!ruby is fun! # => :"ruby is fun"
+p %s(ruby is fun!) # => :"ruby is fun!"
+
+string = "hey"
+p string.to_sym # => :hey
+
+# respond_to? はオブジェクトに対して文字列かシンボルで指定した名前のメソッドを呼び出せるかどうかを調べる
+p 'hey'.respond_to?('include?')
+p 'hey'.respond_to?(:include?)
+
+
+# 自己代入
+limit = nil
+limit ||= 10
+p limit
+
+limit = 20
+limit ||= 10
+p limit
