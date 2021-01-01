9cefbfd957065ee082ccc3e4a228c771c576fae0
takattty <r9r_celtics_t9t@icloud.com>
2021-01-01 23:16:04 +0900

    added super method

--- practice_codes/chapter7/7-3.rb
+++ practice_codes/chapter7/7-3.rb
@@ -7,15 +7,28 @@ class Product
     @name = name
     @price = price
   end
+
+  def to_s
+    "name: #{name}, price: #{price}"
+  end
 end
 
 class DVD < Product
   attr_accessor :running_time
 
   def initialize(name, price, running_time)
-    @name = name
-    @price = price
+    # スーパークラスのinitializeメソッドを呼び出す
+    super(name, price)
     @running_time = running_time
+
+    # スーパークラストサブクラスの引数の数が同じ場合は、引数なしのsuperで渡された引数全てをスーパークラスに渡せる。
+    # 但しsuper()だと引数なしでスーパークラスの同名メソッドを呼び出せる
+
+    # メソッドのオーバーライド
+    # dvd.to_sというto_sメソッドを呼び出してるから、スーパークラスのto_sメソッドを呼び出せるのね。
+    def to_s
+      "#{super}, running_time: #{running_time}"
+    end
   end
 
 end
@@ -25,3 +38,4 @@ dvd = DVD.new('A perfect move', 2000, 120)
 p dvd.name
 p dvd.price
 p dvd.running_time
+p dvd.to_s
