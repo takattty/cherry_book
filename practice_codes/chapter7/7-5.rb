2d9f116f82f4b54025c0a8b3b4c0c32cfc333f06
takattty <r9r_celtics_t9t@icloud.com>
2021-01-04 23:36:30 +0900

    learn class constant and freeze

--- practice_codes/chapter7/7-5.rb
+++ practice_codes/chapter7/7-5.rb
@@ -1,13 +1,24 @@
 # 定数について
 # 定数はメソッドの内部で作成できず、クラス構文の直下で作成する必要あり。
+# 定数は再代入が可能
 
 class Product
   DEFAULT_PRICE = 0
   DEFAULT_COUNT = 0
 
-  private_constant :DEFAULT_COUNT
+
+  DEFAULT_COUNT = 10
+
+  # private_constant :DEFAULT_COUNT
 end
 
 # 参照方法は、クラス名::定数名
 p Product::DEFAULT_PRICE
 p Product::DEFAULT_COUNT # =>  private constant Product::DEFAULT_COUNT referenced (NameError)
+p Product::DEFAULT_COUNT
+Product::DEFAULT_COUNT = 100
+p Product::DEFAULT_COUNT
+
+Product.freeze
+
+Product::DEFAULT_COUNT = 1000 # => can't modify frozen #<Class:Product> (FrozenError)
