394c5c4f9e3128e88b6b81569f3c90d5efbb9063
takattty <r9r_celtics_t9t@icloud.com>
2021-01-11 23:51:17 +0900

    include module

---
+++ practice_codes/chapter8/8-2.rb
@@ -0,0 +1,31 @@
+class Product
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+
+  private
+
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+class User
+  def name
+    log 'name is called'
+    'Alice'
+  end
+
+  private
+
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+product = Product.new
+p product.title
+
+user = User.new
+p user.name
