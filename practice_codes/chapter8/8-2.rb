45672dae71762f6a731ed768f4bbfb8a2eb8c5c5
takattty <r9r_celtics_t9t@icloud.com>
2021-01-12 16:47:11 +0900

    learn about include and extend

--- practice_codes/chapter8/8-2.rb
+++ practice_codes/chapter8/8-2.rb
@@ -1,16 +1,27 @@
 module Loggable
+
+  # ミックスインしたクラスでもprivateメソッドで使用する事になる。
+  # private 
+
   def log(text)
     puts "[LOG] #{text}"
   end
 end
 
-
+# 下記のようにincludeしてクラスに機能を追加することをミックスインという。
 class Product
-  include Loggable
+  # include Loggable
+  # Loggableモジュールのメソッドを特異メソッド（クラスメソッド）としてミックスインする
+  extend Loggable
 
   def title
     log 'title is called.'
     'A great movie'
+  end
+
+  def self.create_products(names)
+    # ログメソッドをクラスメソッド内(self.hoge)で呼び出す。つまりlogメソッドもクラスメソッドになっていると判断出来る。
+    log 'create_products is called'
   end
 end
 
@@ -28,3 +39,8 @@ p product.title
 
 user = User.new
 p user.name
+
+p product.log 'publick?'
+
+p Product.create_products([])
+p Product.log('Hello!')
